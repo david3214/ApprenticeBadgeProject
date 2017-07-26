d9778916d382c3af973a797763a92c97b808dd4a
davomono <davidw3214@gmail.com>
2017-07-26 14:28:00 -0600

    A large amount of updates, mainly focuesd around trying to have the background remembered as you change web pages

--- Project/MainScript.js
+++ Project/MainScript.js
@@ -3,13 +3,36 @@ function changeBackground(){
 
     if(selectbox == "Mountain"){
         document.body.style.backgroundImage = 'url(Pics/Mount-Timpanogos.jpg)';
-        $("#PowellInfo").hide();
-        $("#timpInfo").show();
+        document.getElementById("PowellInfo").style.display = 'none';
+        localStorage.setItem("powellShow","hidden");
+        document.getElementById("timpInfo").style.display = 'block';
+        localStorage.setItem("timpShow","visable");
+        
 
     }
     else{
         document.body.style.backgroundImage = 'url(Pics/LakePowell.jpg)';
-        $("#PowellInfo").show();
-        $("#timpInfo").hide();
+
+        document.getElementById("PowellInfo").style.display = 'block';
+        localStorage.setItem("powellShow","visable");
+
+        document.getElementById("timpInfo").style.display = 'none';
+        localStorage.setItem("timpShow","hidden");
+    }
+}
+function rememberBackground(){
+    //Test to see if my local storage for powellShow is set to visable
+    if(localStorage.getItem("powellShow") == "visable"){
+
+        document.body.classList.toggle('Powell');
+
+        //Supposed to have the option of Lake view on the select instead of mountain view
+        document.getElementById("lakeOption").selected = true;
+    }
+    else{
+        document.body.classList.toggle('Timp');
+        
+        //Supposed to change the default select to Mountain view instead
+        document.getElementById("mountainOption").selected = true;
     }
 }
